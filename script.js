8206512476c07e71d6c0f9f1ef95f0a7c185ad20
prabhjotkaur1665 <prabhjotkaur1665@gmail.com>
2025-08-30 16:04:43 -0700

    added full code

---
+++ script.js
@@ -0,0 +1,18 @@
+let b = document.querySelector("#but")
+b.addEventListener("click", function(){
+let abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let splchar =["!","@","#","$","%","^","&","~"]
+let pass = ""
+
+for (let i = 0; i < 4; i++) {
+    let value = Math.floor(Math.random() * abc.length)
+     let sep = Math.floor(Math.random() * splchar.length)
+        let num = Math.floor(Math.random() * 10)
+
+    pass = pass + abc[value] + splchar[sep] + [num]   
+}
+
+let head= document.getElementsByClassName("fst")
+head[0].innerText = pass
+
+})
