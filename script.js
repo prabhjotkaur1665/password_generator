27dca3cac35a3ab02567f2b7356323bfe044adcc
prabhjotkaur1665 <prabhjotkaur1665@gmail.com>
2025-08-31 12:58:40 -0700

    full code uploaded

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let b = document.querySelector("#but")
 b.addEventListener("click", function(){
+    
 let abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let splchar =["!","@","#","$","%","^","&","~"]
 let pass = ""
